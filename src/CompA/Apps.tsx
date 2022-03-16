483f7b562ccd55b55fd0f9c5fa3efc71fcc812a6
Avinashbawage <avi.bawge9@gmail.com>
2022-03-16 15:49:10 +0530

    API_AXIOS_pokimon

---
+++ src/CompA/Apps.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Apps() {
+  return (
+    <>
+      <h1>your selected number is </h1>
+      <select>
+        <option value="1">1</option>
+        <option value="35">35</option>
+        <option value="45">45</option>
+        <option value="2">2</option>
+        <option value="5">5</option>
+      </select>
+    </>
+  );
+}
+
+export default Apps;
